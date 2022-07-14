17d37069e8e18ae421ab6f700fdb2a4f2fbdef41
Aleksey <1aleksey.mazur@gmail.com>
2022-07-14 03:34:18 +0300

    fix styles.css, header.ejs; move header, footer to partials folder; set page routing in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const port = 3000;
 
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "The French government initially blamed Liverpool fans and fake tickets for the crowd chaos that led to supporters being tear-gassed and robbed. But a Senate report has found authorities blamed them unfairly. Dysfunctional mistakes were made at every level, it said. Two Senate committees investigated what went wrong on the night of the Champions League final between Liverpool and Real Madrid on 28 May, taking evidence from Liverpool fan and club representatives as well as French officials. Liverpool fans have told the BBC that the problems were caused by digital tickets not working properly on the night, leading to problems at the turnstiles.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -17,6 +17,22 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { startingContent: homeStartingContent });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
 
 app.listen(port, () => {
   console.log("Server started on port 3000");
